4feb75501687030312d38d3e847c3f50bcfc1804
Gustavo Santos <gefgu@hotmail.com>
2022-10-04 21:13:38 -0300

    feat: add app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+
 function App() {
-  return <div>Hello World</div>;
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  );
 }
 
 export default App;
